50645ec1087cd4772839f73a5e4decbd4d139694
按照模板填写页面 <chengxiaolin@chengxiaolindeMacBook-Pro.local>
2018-02-21 22:04:20 +0800

    udacity data analyse P1
    exercise 1st

---
+++ Python/advance/DataAnalyse/simple_correct.py
@@ -0,0 +1,23 @@
+DataFile = "daily_engagement.csv"
+
+def parse_file(datafile):
+    data = []
+    with open(datafile,"rb") as f:
+        header = f.readline().split(",")
+        counter = 0
+        for line in f:
+            if counter == 10:
+                break
+
+            fields = line.split(",")
+            entry = {}
+
+            for i,value in enumerate(fields):
+                entry[header[i].strip()] = value.strip()
+
+            data.append(entry)
+            counter += 1
+
+    return data
+
+parse_file(DataFile)
